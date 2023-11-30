4a8e44d145c0f3dadc31c04fadf98b23e8dd01af
KokiKono <kono.koki.pg@gmail.com>
2023-11-30 21:07:43 +0900

    update

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ brew bundle
 mkdir -p "$(nodenv root)/plugins"
 git clone https://github.com/pine/nodenv-yarn-install.git "$(nodenv root)/plugins/nodenv-yarn-install"
 
+# Setup zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+.config/install.sh
